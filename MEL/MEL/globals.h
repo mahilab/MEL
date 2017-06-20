dc8843e2612facfac93c4e631a13aad524cd977f
epezent <epezent@rice.edu>
2017-06-20 11:50:42 -0500

    Test

--- MEL/MEL/globals.h
+++ MEL/MEL/globals.h
@@ -6,12 +6,16 @@
 #define DEG2RAD 1.74532925199433E-02 /* constant to multiply by to convert degrees to radians */
 #define RAD2DEG 5.72957795130824E+01 /* constant to multiply by to convert radians to degrees */
 
+#define TEST 1
+
 typedef unsigned int              uint;
 typedef std::vector<int>          int_vec;
 typedef std::vector<unsigned int> uint_vec;
 typedef std::vector<float>        float_vec;
 typedef std::vector<double>       double_vec;
 typedef std::vector<char>         char_vec;
+
+
 
 /* macro for find array length */
 #define ARRAY_LENGTH(array) (sizeof(array)/sizeof((array)[0]))
