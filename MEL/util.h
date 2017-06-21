bffc5ce73fe1be88deb5e7f48c5f450bee085710
epezent <epezent@rice.edu>
2017-06-20 23:21:05 -0500

    reorganization

--- MEL/util.h
+++ MEL/util.h
@@ -1,16 +1,9 @@
 #pragma once
 #include <vector>
 
-#define PI      3.14159265358979E+00 /* constant pi to double precision */
-#define DEG2RAD 1.74532925199433E-02 /* constant to multiply by to convert degrees to radians */
-#define RAD2DEG 5.72957795130824E+01 /* constant to multiply by to convert radians to degrees */
-
-typedef unsigned int              uint;
-typedef std::vector<int>          int_vec;
-typedef std::vector<unsigned int> uint_vec;
-typedef std::vector<float>        float_vec;
-typedef std::vector<double>       double_vec;
-typedef std::vector<char>         char_vec;
+#define PI      3.14159265358979E+00  /* constant pi */
+#define DEG2RAD 1.74532925199433E-02  /* multiply degrees by this value to convert to radians */
+#define RAD2DEG 5.72957795130824E+01  /* multiply radians by this value to convert to degrees */
 
 /* macro for find array length */
 #define ARRAY_LENGTH(array) (sizeof(array)/sizeof((array)[0]))
@@ -18,3 +11,9 @@ typedef std::vector<char>         char_vec;
 /* a better version of ARRAY_LENGTH */
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
+typedef unsigned int              uint;
+typedef std::vector<int>          int_vec;
+typedef std::vector<unsigned int> uint_vec;
+typedef std::vector<float>        float_vec;
+typedef std::vector<double>       double_vec;
+typedef std::vector<char>         char_vec;
