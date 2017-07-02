29f5dcdad18cd05a0508beb6013be115334f662f
Craig McDonald <craig.g.mcdonald@gmail.com>
2017-07-02 16:53:12 -0500

    Improved Actuator and Motor classes. Added comments. Added EnableMode. Added multiple constructors.

--- MEL/Actuator.cpp
+++ MEL/Actuator.cpp
@@ -3,7 +3,22 @@
 namespace mel {
 
     Actuator::Actuator() :
-        torque_(0.0)
-    {    
+        name_("no name"),
+        torque_(0.0),
+        enable_mode_(EnableMode::None)
+    { }
+
+    Actuator::Actuator(std::string name, EnableMode enable_mode) :
+        name_(name),
+        torque_(0.0),
+        enable_mode_(enable_mode)
+    { }
+
+    void Actuator::enable() {
+        std::cout << "WARNING: Actuator <" << name_ << "> was not constructed to use the function enable()." << std::endl;
+    }
+
+    void Actuator::disable() {
+        std::cout << "WARNING: Actuator <" << name_ << "> was not constructed to use the function disable()." << std::endl;
     }
 }
