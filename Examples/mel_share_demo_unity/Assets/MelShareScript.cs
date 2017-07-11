d50e1ece844f3342d23b47472615a6d09281629a
Evan Pezent <epezent16@gmail.com>
2017-07-11 17:25:17 -0500

    Added back Unity projects

---
+++ Examples/mel_share_demo_unity/Assets/MelShareScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Text;
+
+public class MelShareScript : MonoBehaviour {
+
+    // Import our .dll functions (note that C++ char* array is a byte[] array in C#)
+
+    [DllImport("MELShare", EntryPoint = "read_char_map", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int ReadByteMap(string name, byte[] buffer, int size);
+
+    [DllImport("MELShare", EntryPoint = "read_int_map", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int ReadIntMap(string name, int[] buffer, int size);
+
+    [DllImport("MELShare", EntryPoint = "read_double_map", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int ReadDoubleMap(string name, double[] buffer, int size);
+
+    [DllImport("MELShare", EntryPoint = "write_char_map", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int WriteByteMap(string name, byte[] buffer, int size);
+
+    [DllImport("MELShare", EntryPoint = "write_int_map", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int WriteIntMap(string name, int[] buffer, int size);
+
+    [DllImport("MELShare", EntryPoint = "write_double_map", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+    public static extern int WriteDoubleMap(string name, double[] buffer, int size);
+
+    // Wrap our imported functions into nice, overloaded functions
+
+    public static int ReadMap(string name, byte[] buffer) { return ReadByteMap(name, buffer, buffer.Length); }
+    public static int ReadMap(string name, int[] buffer) { return ReadIntMap(name, buffer, buffer.Length); }
+    public static int ReadMap(string name, double[] buffer) { return ReadDoubleMap(name, buffer, buffer.Length); }
+    public static int WriteMap(string name, byte[] buffer) { return WriteByteMap(name, buffer, buffer.Length); }
+    public static int WriteMap(string name, int[] buffer) { return WriteIntMap(name, buffer, buffer.Length); }
+    public static int WriteMap(string name, double[] buffer) { return WriteDoubleMap(name, buffer, buffer.Length); }
+
+    // Create our data buffers and make them public so they can be seen in the inspector
+
+    public byte[] myBytes = new byte[3];
+    public int[] myInts = new int[5];
+    public double[] myDoubles = new double[10];
+    public int result = 1;
+    
+    // Use this for initialization
+    void Start () {
+
+        print("map0:");
+        result = ReadMap("map0", myBytes);
+        foreach (byte b in myBytes)
+            print((char)b);
+
+        print("map1:");
+        result = ReadMap("map1", myInts);
+        foreach (int i in myInts)
+            print(i);
+
+        print("map2:");
+        result = ReadMap("map2", myDoubles);
+        foreach (double d in myDoubles)
+            print(d);
+
+        myBytes[0] = (byte)'x';
+        myBytes[1] = (byte)'y';
+        myBytes[2] = (byte)'z';
+        result = WriteMap("map0", myBytes);
+
+        for (int i = 0; i < myDoubles.Length; i++)
+            myDoubles[i] *= 2;
+        result = WriteMap("map1", myDoubles);
+
+        myInts = new int[] { 1, 1, 2, 3, 5, 8, 13 };
+        result = WriteMap("map2", myInts);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ OpenWrist/OpenWristVis/Assets/OpenWristScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenWristScript : MonoBehaviour {
+
+    [Header("OpenWrist State Vector")]
+    public double[] state = new double[10];
+
+    [Header("Inertial Frame References")]
+    public GameObject frame0;
+    public GameObject frame1;
+    public GameObject frame2;
+    public GameObject frame3;
+
+    private Vector3 frame0Zero;
+    private Vector3 frame1Zero;
+    private Vector3 frame2Zero;
+    private Vector3 frame3Zero;
+
+    // Use this for initialization
+    void Start () {
+        frame0Zero = frame0.transform.localEulerAngles;
+        frame1Zero = frame1.transform.localEulerAngles;
+        frame2Zero = frame2.transform.localEulerAngles;
+        frame3Zero = frame3.transform.localEulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // read in state from MELShare
+        MelShare.ReadMap("ow_state", state);
+
+        // update frame z-axes
+        frame1.transform.localEulerAngles = new Vector3(frame1Zero.x, frame1Zero.y, -(float)state[1]*Mathf.Rad2Deg);
+        frame2.transform.localEulerAngles = new Vector3(frame2Zero.x, frame2Zero.y, -(float)state[2] * Mathf.Rad2Deg);
+        frame3.transform.localEulerAngles = new Vector3(frame3Zero.x, frame3Zero.y, -(float)state[3] * Mathf.Rad2Deg);
+
+    }
+}
