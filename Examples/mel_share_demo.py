438780210c507bb2d2d2195a142c462b13712b40
Craig McDonald <craig.g.mcdonald@gmail.com>
2017-08-07 14:19:40 -0500

    Updated MelShareExample unity project

--- Examples/mel_share_demo.py
+++ Examples/mel_share_demo.py
@@ -21,21 +21,21 @@ print "Read:"
 
 result = mel_share.read_char_map("map0", my_chars, 3)
 print "map0:    ",
-if (result != 1): print 'ERROR:', result
+if (result < 0): print 'ERROR:', result
 for value in my_chars:
     print value,
 print ' '
 
 result = mel_share.read_int_map("map1", my_ints, 5)
 print "map1:    ",
-if (result != 1): print 'ERROR:', result
+if (result < 0): print 'ERROR:', result
 for value in my_ints:
     print value,
 print ' '
 
 result = mel_share.read_double_map("map2", my_doubles, 10)
 print "map2:    ",
-if (result != 1): print 'ERROR:', result
+if (result < 0): print 'ERROR:', result
 for value in my_doubles:
     print value,
 print ' '
@@ -54,7 +54,7 @@ print ' '
 my_doubles[:] = [value * 2 for value in my_doubles]
 result = mel_share.write_double_map("map1", my_doubles, 10)
 print "map1:    ",
-if (result != 1): print 'ERROR:', result
+if (result < 0): print 'ERROR:', result
 for value in my_doubles:
     print value,
 print ' '
@@ -62,7 +62,7 @@ print ' '
 my_ints = IntArray7(1,1,2,3,5,8,13)
 result = mel_share.write_int_map("map2", my_ints, 7)
 print "map2:    ",
-if (result != 1): print 'ERROR:', result
+if (result < 0): print 'ERROR:', result
 for value in my_ints:
     print value,
 print ' '
