a15cec296fca684ffaa8034b1881abb9f0857bea
Craig McDonald <craig.g.mcdonald@gmail.com>
2017-08-11 17:56:37 -0500

    Added AtiMini45 derived ForceSensor, and added double_mat type to util

--- MEL/ForceSensor.cpp
+++ MEL/ForceSensor.cpp
@@ -4,17 +4,27 @@ namespace mel {
 
     ForceSensor::ForceSensor() :
         name_("no_name"),
-        num_axes_(1)
+        num_channels_(1),
+        voltages_({ 0 })
     { }
 
     ForceSensor::ForceSensor(std::string name, std::vector<Daq::Ai> ai_channels) :
         name_(name),
-        num_axes_(ai_channels.size())
+        num_channels_(ai_channels.size()),
+        voltages_(double_vec(ai_channels.size(),0))
     {
-        for (int i = 0; i < num_axes_; ++i) {
+        for (int i = 0; i < num_channels_; ++i) {
             ai_channels_.push_back(ai_channels[i]);
         }
     }
 
+    double_vec ForceSensor::get_voltages() {
+
+        for (int i = 0; i < num_channels_; ++i) {
+            voltages_[i] = ai_channels_[i].get_voltage();
+        }
+
+        return voltages_;
+    }
 
 }
--- MahiExoII/EmgTraining.cpp
+++ MahiExoII/EmgTraining.cpp
@@ -1,14 +1,14 @@
 #include "EmgTraining.h"
 
 
-EmgTraining::EmgTraining(mel::Clock& clock, mel::Daq* q8_emg, mel::Daq* q8_ati, MahiExoII* exo) :
+EmgTraining::EmgTraining(mel::Clock& clock, mel::Daq* q8_emg, mel::Daq* q8_ati, MahiExoIIFrc* meii) :
     StateMachine(4),
     q8_emg_(q8_emg),
     q8_ati_(q8_ati),
-    exo_(exo),
+    meii_(meii),
     clock_(clock),
     raw_force_(Eigen::VectorXd::Zero(6)),
-    calib_mat_(Eigen::MatrixXd::Zero(6,6)),
+    //calib_mat_(Eigen::MatrixXd::Zero(6,6)),
     calib_force_(Eigen::VectorXd::Zero(6)),
     set_points_({ 0,0,0,0,0 }),
     kp_({ 35, 7, 25, 30, 3500 }),
@@ -27,7 +27,7 @@ EmgTraining::EmgTraining(mel::Clock& clock, mel::Daq* q8_emg, mel::Daq* q8_ati, 
     force_filt_({ 0, 0, 0, 0, 0, 0 })
 
 {
-    calib_mat_ << 0.45676, 0.37263, 2.84454, -95.32922, -1.60986, 93.56974, -10.00557, 107.83272, 2.80461, -54.50607, 2.67834, -55.04209, 133.67479, 5.88886, 131.55424, 5.44438, 134.55104, 5.69414, -0.02942, 0.74195, -2.11485, -0.48201, 2.19007, -0.27703, 2.49045, 0.12279, -1.26019, 0.59413, -1.30218, -0.70275, 0.07348, -1.36804, 0.08441, -1.41171, 0.05780, -1.37930;
+    //calib_mat_ << 0.45676, 0.37263, 2.84454, -95.32922, -1.60986, 93.56974, -10.00557, 107.83272, 2.80461, -54.50607, 2.67834, -55.04209, 133.67479, 5.88886, 131.55424, 5.44438, 134.55104, 5.69414, -0.02942, 0.74195, -2.11485, -0.48201, 2.19007, -0.27703, 2.49045, 0.12279, -1.26019, 0.59413, -1.30218, -0.70275, 0.07348, -1.36804, 0.08441, -1.41171, 0.05780, -1.37930;
 }
 
 void EmgTraining::sf_init(const mel::NoEventData* data) {
@@ -41,7 +41,7 @@ void EmgTraining::sf_init(const mel::NoEventData* data) {
     q8_ati_->activate();
     //std::cout << "Press ENTER to enable MahiExoII" << std::endl;
     //getchar();
-    //exo_->enable();
+    //meii_->enable();
     std::cout << "Press Enter to start the controller" << std::endl;
     getchar();
     q8_emg_->start_watchdog(0.1);
@@ -49,13 +49,13 @@ void EmgTraining::sf_init(const mel::NoEventData* data) {
 
     // get current position
     q8_emg_->read_all();
-    exo_->update_kinematics();
-    init_pos_ = exo_->get_anatomical_joint_positions();
+    meii_->update_kinematics();
+    init_pos_ = meii_->get_anatomical_joint_positions();
     goal_pos_ = init_pos_;
 
     /*
     //write position data (UNITY DEBUG)
-    data_p_ = exo_->get_anatomical_joint_positions();
+    data_p_ = meii_->get_anatomical_joint_positions();
     pos_data_.write(data_p_);
     mel::print(data_p_);
     */
@@ -80,14 +80,14 @@ void EmgTraining::sf_to_neutral(const mel::NoEventData* data) {
 
 
     // enter the control loop
-    while (!ctrl_c_) {
+    /*while (!ctrl_c_) {
 
         q8_emg_->reload_watchdog();
 
         q8_emg_->read_all();
 
 
-        exo_->update_kinematics();
+        meii_->update_kinematics();
 
         // set torques
         init_time_ = 0;
@@ -95,23 +95,23 @@ void EmgTraining::sf_to_neutral(const mel::NoEventData* data) {
 
             x_ref_[i] = moving_set_point(init_pos_[i], goal_pos_[i], init_time_, clock_.time(), speed_[i]);
 
-            new_torques_[i] = mel::pd_controller(kp_[i], kd_[i], x_ref_[i], exo_->get_anatomical_joint_position(i), 0, exo_->get_anatomical_joint_velocity(i));
+            new_torques_[i] = mel::pd_controller(kp_[i], kd_[i], x_ref_[i], meii_->get_anatomical_joint_position(i), 0, meii_->get_anatomical_joint_velocity(i));
 
             if (backdrive_[i] == 1) {
                 new_torques_[i] = 0;
             }
 
         }
-        /*
-        data_p_ = exo_->get_anatomical_joint_positions();
+        
+        data_p_ = meii_->get_anatomical_joint_positions();
         pos_data_.write(data_p_);
-        mel::print(data_p_);*/
+        mel::print(data_p_);
 
-        //mel::print(exo_->get_anatomical_joint_position(0));
+        //mel::print(meii_->get_anatomical_joint_position(0));
         //mel::print(new_torques_[2]);
-        exo_->set_anatomical_joint_torques(new_torques_);
+        meii_->set_anatomical_joint_torques(new_torques_);
         q8_emg_->write_all();
-    }
+    }*/
 
 }
 
@@ -124,7 +124,7 @@ void EmgTraining::sf_hold_neutral(const mel::NoEventData* data) {
 
 
     // enter the control loop
-    while (!ctrl_c_) {
+    //while (!ctrl_c_) {
 
 
         q8_emg_->reload_watchdog();
@@ -132,7 +132,9 @@ void EmgTraining::sf_hold_neutral(const mel::NoEventData* data) {
         q8_emg_->read_all();
         q8_ati_->read_all();
 
-        std::vector<double> v = q8_ati_->get_analog_voltages();
+
+
+        /*std::vector<double> v = q8_ati_->get_analog_voltages();
         double* ptr = &v[0];
         Eigen::Map<Eigen::VectorXd> raw_force(ptr, 6);
 
@@ -140,12 +142,15 @@ void EmgTraining::sf_hold_neutral(const mel::NoEventData* data) {
 
         calib_force_ = calib_mat_*raw_force_;
         force_share_.resize(calib_force_.size());
-        Eigen::VectorXd::Map(&force_share_[0], calib_force_.size()) = calib_force_;
+        Eigen::VectorXd::Map(&force_share_[0], calib_force_.size()) = calib_force_;*/
 
-        force_filt_ = multi_lpf_.filter(force_share_);
+        force_share_ = meii_->wrist_force_sensor_->get_forces();
+        mel::print(force_share_);
 
-        ati_data_.write(force_filt_);
-    }
+        //force_filt_ = multi_lpf_.filter(force_share_);
+
+        //ati_data_.write(force_filt_);
+    //}
 
 }
 
