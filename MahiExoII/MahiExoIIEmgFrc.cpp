44037f6e5f9ca29177af98c79303f72d84916295
Craig McDonald <craig.g.mcdonald@gmail.com>
2017-09-09 18:47:03 -0500

    moving MahiExoII files to MEL

--- MahiExoII/MahiExoIIEmgFrc.cpp
+++
@@ -1,7 +0,0 @@
-#include "MahiExoIIEmgFrc.h"
-
-MahiExoIIEmgFrc::MahiExoIIEmgFrc(Config configuration, Params parameters) :
-    MahiExoII(configuration, parameters),
-    MahiExoIIEmg(configuration, parameters),
-    MahiExoIIFrc(configuration, parameters)
-{}
