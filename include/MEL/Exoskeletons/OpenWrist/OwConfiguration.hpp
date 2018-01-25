6e9a6754b046158450a1f5a5160669049ecbb3a6
epezent <epezent@rice.edu>
2018-01-25 15:37:47 -0600

    added VoltPaqX4

--- include/MEL/Exoskeletons/OpenWrist/OwConfiguration.hpp
+++ include/MEL/Exoskeletons/OpenWrist/OwConfiguration.hpp
@@ -20,11 +20,12 @@
 #include <MEL/Core/Amplifier.hpp>
 #include <MEL/Daq/Daq.hpp>
 #include <MEL/Daq/Encoder.hpp>
-#include <MEL/Daq/Velocity.hpp>
 #include <MEL/Daq/Input.hpp>
 #include <MEL/Daq/Output.hpp>
+#include <MEL/Daq/Velocity.hpp>
 #include <MEL/Daq/Watchdog.hpp>
 #include <vector>
+
 
 namespace mel {
 
@@ -33,6 +34,7 @@ namespace mel {
 //==============================================================================
 
 class OpenWrist;
+class Q8Usb;
 
 //==============================================================================
 // CLASS DECLARATION
@@ -40,49 +42,49 @@ class OpenWrist;
 
 /// Encapsulates the hardware configuration for an OpenWrist
 struct OwConfiguration {
-
 public:
+    /// Generic Configuration 1 (creates Amplifiers)
+    OwConfiguration(Daq& daq,
+                    Watchdog& watchdog,
+                    const std::vector<Amplifier::TtlLevel>& enable_levels,
+                    const std::vector<DigitalOutput::Channel>& enable_channels,
+                    const std::vector<double>& command_gains,
+                    const std::vector<AnalogOutput::Channel>& command_channels,
+                    const std::vector<Limiter>& amp_current_limiters,
+                    const std::vector<Amplifier::TtlLevel>& fault_levels,
+                    const std::vector<DigitalInput::Channel>& fault_channels,
+                    const std::vector<double>& sense_gains,
+                    const std::vector<AnalogInput::Channel>& sense_channel,
+                    const std::vector<Encoder::Channel>& encoder_channels,
+                    const std::vector<Velocity::Channel>& velocity_channels);
 
-    /// Constructor Variant 1 (creates Amplifiers)
-    OwConfiguration(
-        Daq& daq,
-        Watchdog& watchdog,
-        const std::vector<Amplifier::TtlLevel>& enable_levels,
-        const std::vector<DigitalOutput::Channel>& enable_channels,
-        const std::vector<double>& command_gains,
-        const std::vector<AnalogOutput::Channel>& command_channels,
-        const std::vector<Limiter>& amp_current_limiters,
-        const std::vector<Amplifier::TtlLevel>& fault_levels,
-        const std::vector<DigitalInput::Channel>& fault_channels,
-        const std::vector<double>& sense_gains,
-        const std::vector<AnalogInput::Channel>& sense_channel,
-        const std::vector<Encoder::Channel>& encoder_channels,
-        const std::vector<Velocity::Channel>& velocity_channels);
+    /// Generic Configuration 2 (given Amplifiers)
+    OwConfiguration(Daq& daq,
+                    Watchdog& watchdog,
+                    const std::vector<Encoder::Channel>& encoder_channels,
+                    const std::vector<Velocity::Channel>& velocity_channels,
+                    const std::vector<Amplifier>& amplifiers);
 
-    /// Constructor Variant 2 (given Amplifiers)
-    OwConfiguration(
-        Daq& daq,
-        Watchdog& watchdog,
-        const std::vector<Encoder::Channel>& encoder_channels,
-        const std::vector<Velocity::Channel>& velocity_channels,
-        const std::vector<Amplifier>& amplifiers);
+    /// Q8Usb + VoltPAQ-X4 Configuration
+    OwConfiguration(Q8Usb& q8_usb);
 
 private:
 
     friend class OpenWrist;
 
-    Daq&                            daq_;                ///< DAQ controlling the OpenWrist
-    Watchdog&                       watchdog_;           ///< watchdog the OpenWrist is guarded by
-    std::vector<Encoder::Channel>   encoder_channels_;   ///< encoder channels that measure motor positions
-    std::vector<Velocity::Channel>  velocity_channels_;  ///< encoder channels that measure motor velocities
-    std::vector<Amplifier>          amplifiers_;         ///< the amplifiers being use to control the OpenWrist's motors
-
-
+    Daq& daq_;            ///< DAQ controlling the OpenWrist
+    Watchdog& watchdog_;  ///< watchdog the OpenWrist is guarded by
+    std::vector<Encoder::Channel>
+        encoder_channels_;  ///< encoder channels that measure motor positions
+    std::vector<Velocity::Channel>
+        velocity_channels_;  ///< encoder channels that measure motor velocities
+    std::vector<Amplifier> amplifiers_;  ///< the amplifiers being use to
+                                         ///< control the OpenWrist's motors
 };
 
-} // namespace mel
+}  // namespace mel
 
-#endif // MEL_OWCONFIGURATION_HPP
+#endif  // MEL_OWCONFIGURATION_HPP
 
 //==============================================================================
 // CLASS DOCUMENTATION
--- src/MEL/Exoskeletons/OpenWrist/OwConfiguration.cpp
+++ src/MEL/Exoskeletons/OpenWrist/OwConfiguration.cpp
@@ -1,4 +1,7 @@
 #include <MEL/Exoskeletons/OpenWrist/OwConfiguration.hpp>
+#ifdef MEL_INCLUDE_QUANSER
+#include <MEL/Daq/Quanser/Q8Usb.hpp>
+#endif
 
 namespace mel {
 
@@ -6,7 +9,7 @@ namespace mel {
 // CLASS DEFINTIONS
 //==============================================================================
 
-    OwConfiguration::OwConfiguration(
+OwConfiguration::OwConfiguration(
     Daq& daq,
     Watchdog& watchdog,
     const std::vector<Amplifier::TtlLevel>& enable_levels,
@@ -19,25 +22,17 @@ namespace mel {
     const std::vector<double>& sense_gains,
     const std::vector<AnalogInput::Channel>& sense_channels,
     const std::vector<Encoder::Channel>& encoder_channels,
-    const std::vector<Velocity::Channel>& velocity_channels) :
-    daq_(daq),
-    watchdog_(watchdog),
-    encoder_channels_(encoder_channels),
-    velocity_channels_(velocity_channels)
-{
+    const std::vector<Velocity::Channel>& velocity_channels)
+    : daq_(daq),
+      watchdog_(watchdog),
+      encoder_channels_(encoder_channels),
+      velocity_channels_(velocity_channels) {
     for (std::size_t i = 0; i < 3; ++i) {
         amplifiers_.push_back(
-            Amplifier("ow_amp_" + std::to_string(i),
-                enable_levels[i],
-                enable_channels[i],
-                command_gains[i],
-                command_channels[i],
-                amp_current_limiters[i],
-                fault_levels[i],
-                fault_channels[i],
-                sense_gains[i],
-                sense_channels[i]
-            ));
+            Amplifier("ow_amp_" + std::to_string(i), enable_levels[i],
+                      enable_channels[i], command_gains[i], command_channels[i],
+                      amp_current_limiters[i], fault_levels[i],
+                      fault_channels[i], sense_gains[i], sense_channels[i]));
     }
 }
 
@@ -46,14 +41,29 @@ OwConfiguration::OwConfiguration(
     Watchdog& watchdog,
     const std::vector<Encoder::Channel>& encoder_channels,
     const std::vector<Velocity::Channel>& velocity_channels,
-    const std::vector<Amplifier>& amplifiers) :
-    daq_(daq),
-    watchdog_(watchdog),
-    encoder_channels_(encoder_channels),
-    velocity_channels_(velocity_channels),
-    amplifiers_(amplifiers)
-{
-}
+    const std::vector<Amplifier>& amplifiers)
+    : daq_(daq),
+      watchdog_(watchdog),
+      encoder_channels_(encoder_channels),
+      velocity_channels_(velocity_channels),
+      amplifiers_(amplifiers) {}
 
+}  // namespace mel
 
-} // namespace mel
+#ifdef MEL_INCLUDE_QUANSER
+OwConfiguration::OwConfiguration(Q8Usb& q8_usb)
+    : OwConfiguration(
+          q8_usb,
+          q8_usb.watchdog,
+          std::vector<Amplifier::TtlLevel>(3, Amplifier::TtlLevel::High),
+          q8_usb.digital_output[{0, 1, 2}],
+          {1.0, 1.0, 1.0},
+          q8_usb.analog_output[{0, 1, 2}],
+          {Limiter(), Limiter(), Limiter()},
+          std::vector<Amplifier::TtlLevel>(3, Amplifier::TtlLevel::High),
+          q8_usb.digital_input[{0, 1, 2}],
+          {1.0, 1.0, 1.0},
+          q8_usb.analog_input[{0, 1, 2}],
+          q8_usb.encoder[{0, 1, 2}],
+          q8_usb.velocity[{0, 1, 2}]) {}
+#endif
