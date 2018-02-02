717b62115b97d5d8700e04ee475280bde17c88f4
Evan Pezent <epezent@rice.edu>
2018-02-01 22:56:42 -0600

    update lic

--- include/MEL/Math/Integrator.hpp
+++ include/MEL/Math/Integrator.hpp
@@ -1,5 +1,6 @@
 // MIT License
 //
+// MEL - MAHI Exoskeleton Library
 // Copyright (c) 2018 Mechatronics and Haptic Interfaces Lab - Rice University
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
