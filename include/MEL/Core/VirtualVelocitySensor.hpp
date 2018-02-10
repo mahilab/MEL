8503cfdaa3791a8094dc1ea1932f1afd6e5b0e7d
epezent <epezent@rice.edu>
2018-02-09 18:00:14 -0600

    add VirtualVelocitySensor

---
+++ include/MEL/Core/VirtualVelocitySensor.hpp
@@ -0,0 +1,43 @@
+// MIT License
+//
+// MEL - MAHI Exoskeleton Library
+// Copyright (c) 2018 Mechatronics and Haptic Interfaces Lab - Rice University
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// Author(s): Evan Pezent (epezent@rice.edu)
+//            Craig McDonald (craig.g.mcdonald@gmail.com)
+//
+
+#ifndef MEL_VIRTUALVELOCITYSENSOR_HPP
+#define MEL_VIRTUALVELOCITYSENSOR_HPP
+
+#include <MEL/Core/VelocitySensor.hpp>
+#include <MEL/Core/PositionSensor.hpp>
+#include <MEL/Math/SignalProcessor.hpp>
+#include <MEL/Math/Differentiator.hpp>
+#include <MEL/Math/Filter.hpp>
+
+namespace mel {
+
+class VirtualVelocitySensor : public VelocitySensor {
+    public:
+
+        VirtualVelocitySensor(PositionSensor& position_sensor, const Filter& filter = Filter({ 1,0 }, { 1, 0 }));
+
+    private:
+
+        /* data */
+};
+
+} // mel
+
+#endif // MEL_VIRTUALVELOCITYSENSOR_HPP
