95638858cf225ded8988bab877d58f8fd89d3b1b
epezent <epezent@rice.edu>
2018-04-24 22:31:54 -0500

    engine dev

--- tests/testing4.cpp
+++ tests/testing4.cpp
@@ -1,64 +1,49 @@
-#include <vector>
-#include <memory>
 #include <iostream>
+#include <memory>
+#include <vector>
 
-
-class VirtualBase {  // Really am not sure what it should be called, sorry
+class VirtualBase {
     class Interface {
     public:
         virtual ~Interface() = default;
         virtual double get() = 0;
     };
 
-    template<typename T>
+    template <typename T>
     class Impl : public Interface {
         T m_impl_obj;
 
     public:
         Impl(T impl_obj) : m_impl_obj(std::move(impl_obj)) {}
 
-        double get() override {
-            return m_impl_obj.get();
-        }
+        double get() override { return m_impl_obj.get(); }
     };
 
     std::shared_ptr<Interface> m_obj;
 
 public:
-    template<typename T>
+    template <typename T>
     VirtualBase(T obj) : m_obj(new Impl<T>(std::move(obj))) {}
 
-    double get() {
-        return m_obj->get();
-    }
+    double get() { return m_obj->get(); }
 };
 
-
-template <typename Derived>
-class Base{
+class DerivedA {
 public:
-    double get(){
-        return static_cast<Derived*>(this)->implementation();
-    }
+    double get() { return 2.0; }
 };
 
-class DerivedA : public Base<DerivedA>{
+class DerivedB {
 public:
-    double get(){ return 2.0;}
+    double get() { return 1.0; }
 };
-
-class DerivedB : public Base<DerivedB>{
-public:
-    double get(){ return 1.0;}
-};
-
 
 int main() {
     std::vector<VirtualBase> v;
     v.emplace_back(DerivedA{});
     v.emplace_back(DerivedB{});
 
-    for(auto b : v) {
+    for (auto b : v) {
         std::cout << b.get() << std::endl;
     }
 }
