7d1ac76875b74e73c311aede2a47810214589c26
Matthew E Pezent <mep9@ad.rice.edu>
2019-02-10 16:20:24 -0600

    update myrio example

--- examples/ex_myrio.cpp
+++ examples/ex_myrio.cpp
@@ -23,36 +23,66 @@
 #include <MEL/Communications/MelNet.hpp>
 #include <string>
 #include <MEL/Logging/Log.hpp>
+#include <MEL/Math/Constants.hpp>
 
 using namespace mel;
 using namespace std;
 
+// create global stop variable CTRL-C handler function
+ctrl_bool stop(false);
+bool handler(CtrlEvent event) {
+    if (event == CtrlEvent::CtrlC || event == CtrlEvent::Close) {
+        stop = true;
+        print("Application Terminated");
+    }
+    return true;
+}
+
+// main
 int main(int argc, char** argv) {
 
-    MelNet mn(55002, 55001, "192.168.1.7");
+    // register handler to gracefully exit program
+    register_ctrl_handler(handler);
 
+    // MelNet to send data to host PC (port 55001@172.22.11.1 from local port 55002)
+    MelNet mn(55002, 55001, "172.22.11.1");
+
+    // create MyRio object
     MyRio myrio("myrio");
-    myrio.open();
-    myrio.enable();
-    Waveform sinwave(Waveform::Sin, seconds(1), 5);
+    if (!myrio.open() || !myrio.enable())
+        return 1;
 
-    std::vector<double> data(2);
+    // set units per count on encoder
+    myrio.C.encoder[0].set_units_per_count(2*PI/500.0f);
 
+    // create sinwave for loopback
+    Waveform sinwave(Waveform::Sin, seconds(1), 10);
+
+    // create loop Timer and Time t
     Timer timer(hertz(1000));
     Time t;
-    while ((t = timer.get_elapsed_time()) < seconds(60)) {
+
+    // loop
+    while (!stop) {
+        t = timer.get_elapsed_time_actual();
+        // update myrio inputs
         myrio.update_input();
-
-        data[0] = sinwave(t);
-        myrio.C.AO[0].set_value(data[0]);
-        data[1] = myrio.C.AI[0];
-
-        print(myrio.C.encoder[0].get_value());
-
-
-
+        // perform analog loopback
+        double voltage_write = sinwave(t);
+        myrio.C.AO[0].set_value(voltage_write);
+        double voltage_read  = myrio.C.AI[0].get_value();
+        // read an encoder
+        double position = myrio.C.encoder[0].get_position();
+        // send myRIO encoder position over MelNet
+        mn.send_data({voltage_write, voltage_read, position});
+        // update myrio outputs
         myrio.update_output();
+        // wait timer
         timer.wait();
     }
 
+    // shutdown myrio
+    myrio.disable();
+    myrio.close();
+    return 0;
 }
